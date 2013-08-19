7a25adcf6440aceacc229144c9ea5b1025e0bf3d
Diego Vargas <degva.29@gmail.com>
2013-08-18 19:00:51 -0500

    Test commit

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 	<head>
 	</head>
 	<body>
-		
+		<div id='menu-head'>
+			<h3>Altopolimero</h3>
+			<ul>
+				<li>Inicio</li>
+				<li>Servicios</li>
+				<li>Proyectos</li>
+				<li>Capacitaciones</li>
+				<li>Quienes Somos</li>
+				<li>Contactenos</li>
+			</ul>
+		</div>		
 	</body>
 </html>
