ca8f833167771b199c9eb34e645a006a140587b4
Diego Vargas <degva.29@gmail.com>
2013-12-08 23:42:10 -0500

    Worked on the navbar

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     <div id="wrap">
 
       <!-- Fixed navbar -->
-      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="colortochange navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -38,11 +38,11 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Altopolimero - logo</a>
+            <a class="navbar-brand" href="#"><img src='img/logo-color.png' alt='ALTOPOLIMERO' style='height: 50px'></a>
           </div>
           <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="#home">Inicio</a></li>
+              <li class="active"><a href="#home">Altopolimero</a></li>
               <li><a href="#services">Servicios</a></li>
               <li><a href="#proyects">Proyectos</a></li>
               <li><a href="#training">Capacitacion</a></li>
@@ -73,8 +73,10 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <!-- <script src='https://code.jquery.com/ui/1.10.3/jquery-ui.js'></script> -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script> 
     <script src="dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/main-alt.js"></script>
   </body>
 </html>
 
