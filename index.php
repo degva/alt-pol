a5ca17f21b097d6619f974c07fc69754e5502c3f
Diego Vargas <degva.29@gmail.com>
2014-01-12 17:47:40 -0500

    Added Carousel

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<!--
-		<link href='assets/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
-		<link href='assets/css/bootstrap-responsive.min.css' rel='stylesheet' type='text/css'>
-		-->
+		<link href='css/bootstrap.min.css' rel='stylesheet' type='text/css'>
 		<link href='css/menu.css' rel='stylesheet' type='text/css'>
 		<link href='css/content.css' rel='stylesheet' type='text/css'>
 	</head>
@@ -13,11 +10,53 @@
 		<div class='content'>
 			<div class='hr' ></div>
 			<p>CONTENIDO!</p>
+
+
+      <div id="carousel-example-captions" class="carousel slide" data-ride="carousel">
+
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-captions" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-captions" data-slide-to="1"></li>
+          <li data-target="#carousel-example-captions" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="http://placehold.it/800x400" alt="800x500">
+            <div class="carousel-caption">
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="http://placehold.it/800x400" alt="800x500">
+            <div class="carousel-caption">
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="http://placehold.it/800x400" alt="800x500">
+            <div class="carousel-caption">
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </div>
+          </div>
+        </div>
+
+        <a class="left carousel-control" href="#carousel-example-captions" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-captions" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+
+      </div>
+    
+
 		</div>
 	</body>
 	<?php include "includes/stylesscripts.php"; ?>
-	<!--
-	<script src='assets/js/bootstrap.min.js'></script>
-	-->
+	<script src='js/bootstrap.min.js'></script>
 </html>
 
