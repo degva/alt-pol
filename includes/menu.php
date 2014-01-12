6ceeac03e8fc2ced2e46f0a940a49944af6687b3
Diego Vargas <degva.29@gmail.com>
2014-01-12 16:14:25 -0500

    Made the menu

---
+++ includes/menu.php
@@ -0,0 +1,27 @@
+<div class='menu'>
+	<div class='logo'>
+		<img src='img/altologo.png' width="250px"/>
+	</div>
+	<div class='top-catch'>
+		DESARROLLO DE TECNOLOGÍAS A LA VANGUARDIA
+	</div>
+	<div class='social'>
+		<img src='http://placehold.it/150x120'/>
+	</div>
+	<nav>
+		<ul>
+			<li><a href='#'>SERVICIOS</a>
+				<ul class='dropdown-menu'>	
+					<li><a href='#'>LINEA INDUSTRIAL</a></li>
+					<li><a href='#'>LINEA DECORATIVA</a></li>
+					<li><a href='#'>CLIENTES ATENDIDOS</a></li>
+				</ul>
+			</li>
+			<li><a href='#'>PROYECTOS</a></li>
+			<li><a href='#'>CAPACITACIONES</a></li>
+			<li><a href='#'>QUIENES SOMOS</a></li>
+			<li><a href='#'>CONTACTENOS</a></li>
+		</ul>
+	</nav>
+
+</div>
--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 	<head>
+		<!--
+		<link href='assets/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+		<link href='assets/css/bootstrap-responsive.min.css' rel='stylesheet' type='text/css'>
+		-->
+		<link href='css/menu.css' rel='stylesheet' type='text/css'>
+		<link href='css/content.css' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-		<div id='menu-head'>
-			<h3>Altopolimero</h3>
-			<ul>
-				<li>Inicio</li>
-				<li>Servicios</li>
-				<li>Proyectos</li>
-				<li>Capacitaciones</li>
-				<li>Quienes Somos</li>
-				<li>Contactenos</li>
-			</ul>
-		</div>		
+		<?php include "includes/menu.php"; ?>
+		<div class='content'>
+			<div class='hr' ></div>
+			<p>CONTENIDO!</p>
+		</div>
 	</body>
+	<?php include "includes/stylesscripts.php"; ?>
+	<!--
+	<script src='assets/js/bootstrap.min.js'></script>
+	-->
 </html>
