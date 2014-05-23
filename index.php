63c20e1ffa70a4f29d69ad7c62787e3aa6622fdf
Diego Vargas <degva.29@gmail.com>
2014-05-23 06:51:36 -0500

    Agregue inico

--- index.php
+++ index.php
@@ -18,7 +18,27 @@
 			<!-- START MENU -->
 			<?php include 'includes/menu.php' ?>
 			<!-- END MENU -->
+			<!-- START INIT -->
+			<br />
+			<div id='init' class='center width-pri'>
+				<h4 class='center width-pri'>Una empresa dedicada a las necesidades del cliente</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> Duis mattis sit amet massa quis suscipit. Etiam libero sapien, facilisis in. </p>
+				<div id='bubbles' class='center width-pri'>
+					<div class='bub' target='industrial' onclick="document.location.href = '#industrial'">
+						<img src='http://placehold.it/200x200'>
+						<small class='center'>Linea Industrial</small>
+					</div>
+					<div class='bub' target='publicitaria' onclick="document.location.href = '#publicitaria'">
+						<img src='http://placehold.it/200x200'>
+						<small class='center'>Linea Publicitaria</small>
+					</div>
+					<div class='bub' target='decorativa' onclick="document.location.href = '#decorativa'">
+						<img src='http://placehold.it/200x200'>
+						<small class='center'>Linea Decorativa</small>
+					</div>
+				</div>
 			</div>
+			<!-- END INIT -->
 		</div>
 	</body>
 	<script src="//code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
