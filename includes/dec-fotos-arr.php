83e8477b4b669d438db2263c9aa41703de1728fd
Diego Vargas <degva.29@gmail.com>
2014-06-24 11:14:34 -0500

    Agregue info

--- includes/dec-fotos-arr.php
+++ includes/dec-fotos-arr.php
@@ -2,32 +2,80 @@
 	$fotos = array(
 		"fotos" => array('d01','d02','d03'),
 		"d01" => array(
-				"info" => "Foto uno dec",
-				"data" => "http://placehold.it/150x150",
-				"informacion" => "This that and there 1 dec",
-				"descrip" => "Esto es muy loco, foto uno dec",
+				"info" => "Android - Dummie 3D",
+				"data" => "img/decorativo/android/thumb.jpg",
+				"informacion" => "Android 3D hecho en fibra de vidrio",
+				"descrip" => "Apoyamos diseños de campañas publicitarias. Ponemos a su disposición las mejores y últimas técnicas de acabados.<br>Cliente: SPAM",
 				"fotosExtras" => array(
 					"http://placehold.it/700x400",
 					"http://placehold.it/700x400",
 				)
 			),
 		"d02" => array(
-				"info" => "Foto dos dec",
-				"data" => "http://placehold.it/150x150",
-				"informacion" => "This that and there 2 dec",
-				"descrip" => "Esto es muy loco, foto dos dec",
+				"info" => "Animales - Dummies 3D",
+				"data" => "img/decorativo/animales-divercity/thumb.jpg",
+				"informacion" => "Animales mecatronicos 3D hechos en fibra de vidrio",
+				"descrip" => "Siempre que Ud. busque animatrónicos realistas, aquí estamos. Desde productos controlados desde su computadora, controles PLC , diseñamos sus placas electrónicas para cada necesidad, mecanismos prácticos y rendidores. Movimiento iluminnación y audio. Piezas en PRFV, siliconas, acrilicos, efectos perlecentes, colores orgánicos inertes, estructuras y mecanismos de calidad. Exportamos. <br>Cliente: Divercity",
 				"fotosExtras" => array(
 					"http://placehold.it/700x400",
 					"http://placehold.it/700x400",
-					"http://placehold.it/700x400",
-					"http://placehold.it/700x400"
 				)
 			),
 		"d03" => array(
-				"info" => "Foto tres dec",
-				"data" => "http://placehold.it/150x150",
-				"informacion" => "This that and there 3 dec",
-				"descrip" => "Esto es muy loco, foto tres dec",
+				"info" => "Auto Checker - Dummie 3D",
+				"data" => "img/decorativo/auto-checker/thumb.jpg",
+				"informacion" => "Auto Checker decorativo para fachada de restaurante hecho en fibra de vidrio con piezas metalizadas",
+				"descrip" => "Si su propuesta es retro, pues aqui la tenemos. Arte e Ingeniería a su alcance,	moldeo, termoformado, metalizados en niquel y cromo sobre fibra de vidrio. Como ninguno, nosotros disponemos de las tecnologías para su proyecto. Cualquier cosa que imagine su departamente de diseño. Corto tiempo de produccion, bajo costo e innovación.<br>Cliente: SPAM",
+				"fotosExtras" => array(
+					"http://placehold.it/700x400",
+					"http://placehold.it/700x400",
+				)
+			),
+		"d04" => array(
+				"info" => "Autos Motorizados - 3D",
+				"data" => "img/decorativo/autos-divercity/thumb.jpg",
+				"informacion" => "Autos Motorizados hechos en fibra de vidrio con motor para movilizacion",
+				"descrip" => "Si su parque necesita carros a batería para diversión para niños o adultos. Traíganos su propuesta. Excelentes acabados, durabilidad, amistosos con el ambiente, soporte técnico.<br>Cliente: Divercity",
+				"fotosExtras" => array(
+					"http://placehold.it/700x400",
+					"http://placehold.it/700x400",
+				)
+			),
+		"d05" => array(
+				"info" => "Caracol \"Turbo\" - 3D",
+				"data" => "img/decorativo/caracol-turbo/thumb.jpg",
+				"informacion" => "Caracol \"Turbo\" decorativo para mostrador en cine",
+				"descrip" => "Va a estrenar una película?. Pues aquí tenemos lo que busca: <b>rapidéz, bajo costo y perfección</b>. Eficacia de impacto con efectividad 1000 por ciento. Vallas móviles.<br>Cliente: SPAM",
+				"fotosExtras" => array(
+					"http://placehold.it/700x400",
+					"http://placehold.it/700x400",
+				)
+			),
+		"d06" => array(
+				"info" => "Escultura",
+				"data" => "img/decorativo/esculturas/thumb.jpg",
+				"informacion" => "Escultura en base de espuma reforzado con fibra de vidrio",
+				"descrip" => "Museos, expo de artes, proyectos de artistas, tienen su casa: Altopolimero <br> Cliente: Divercity",
+				"fotosExtras" => array(
+					"http://placehold.it/700x400",
+					"http://placehold.it/700x400",
+				)
+			),
+		"d07" => array(
+				"info" => "Representacion de Industrias - Dummie 3D",
+				"data" => "img/decorativo/maquinaria-divercity/thumb.jpg",
+				"informacion" => "Representacion con funcionalidad de procesos industriales controlado por computadoras",
+				"descrip" => "Dummies de fábricas, funcionamiento de ficción, educativos y divertidos. Excelentes acabados, durabilidad, arte e ingeniería. <br>Cliente: Divercity",
+				"fotosExtras" => array(
+					"http://placehold.it/700x400",
+					"http://placehold.it/700x400",
+				)
+			),
+		"d08" => array(
+				"info" => "Piezas decorativas 3D",
+				"data" => "img/decorativo/piezas-decorativas/thumb.jpg",
+				"informacion" => "Piezas decorativas hechas en fibra de vidrio con acabados varios",
+				"descrip" => "Disponemos de planta para metalizar sus modelos y moldes. Cobreado, niquelado, cromado, plateado. Traíganos sus diseños, nosotros lo metalizamos. Unicos en el Perú.<br>Cliente: Varios",
 				"fotosExtras" => array(
 					"http://placehold.it/700x400",
 					"http://placehold.it/700x400",
