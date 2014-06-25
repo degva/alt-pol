cf91301c0951d7620e8f990ed92942f8d9601c4e
Diego Vargas <degva.29@gmail.com>
2014-06-25 16:05:02 -0500

    Fotos para industrial!

--- includes/ind-fotos-arr.php
+++ includes/ind-fotos-arr.php
@@ -1,38 +1,61 @@
 <?php
 	$fotos = array(
-		"fotos" => array('i01','i02','i03'),
+		"fotos" => array('i01','i02','i03','i04'),
 		"i01" => array(
-				"info" => "Foto uno",
-				"data" => "http://placehold.it/150x150",
-				"informacion" => "This that and there 1",
-				"descrip" => "Esto es muy loco, foto uno ind",
+				"info" => "Botes en PRFV",
+				"data" => "img/industrial/botes/thumb.jpg",
+				"informacion" => "Bote construido en fibra de vidrio reforzado",
+				"descrip" => "Estructuras sólidas, lineas sencillas pero efectivas para cada necesidad. PRFV de Altopolímero y naútica. Rapidez. Bajo costo, garantía.",
 				"fotosExtras" => array(
-					"http://placehold.it/700x400",
-					"http://placehold.it/700x400",
+					"img/industrial/botes/001.jpg",
+					"img/industrial/botes/002.jpg",
+					"img/industrial/botes/003.jpg"
 				)
 			),
 		"i02" => array(
-				"info" => "Foto dos",
-				"data" => "http://placehold.it/150x150",
-				"informacion" => "This that and there 2",
-				"descrip" => "Esto es muy loco, foto dos ind",
+				"info" => "Clarificadores",
+				"data" => "img/industrial/clarificadores/thumb.jpg",
+				"informacion" => "Clarificadores hechos en fibra de vidrio antiacida",
+				"descrip" => "Fabricamos equipos de sedimentación, probetas para lodos alcalinos o ácidos.",
 				"fotosExtras" => array(
-					"http://placehold.it/700x400",
-					"http://placehold.it/700x400",
-					"http://placehold.it/700x400",
-					"http://placehold.it/700x400"
+					"img/industrial/clarificadores/001.jpg",
+					"img/industrial/clarificadores/002.jpg",
+					"img/industrial/clarificadores/003.jpg"
 				)
 			),
 		"i03" => array(
-				"info" => "Foto tres",
-				"data" => "http://placehold.it/150x150",
-				"informacion" => "This that and there 3",
-				"descrip" => "Esto es muy loco, foto tres ind",
+				"info" => "Piezas Industriales",
+				"data" => "img/industrial/piezas-industriales/thumb.jpg",
+				"informacion" => "Piezas hechas de polimeros resistentes a duras tareas y fibra de vidrio",
+				"descrip" => "Cobertores, canaletas, agitadores, impellers en PRFV, epoxivinil ester,	polipropilenos, PMMA, PVC, Policarbonatos, de diverso requerimientos industrial. Baldosas antiacidas de polímero y alto tránsito.",
 				"fotosExtras" => array(
-					"http://placehold.it/700x400",
-					"http://placehold.it/700x400",
-					"http://placehold.it/700x400"
+					"img/industrial/piezas-industriales/001.jpg",
+					"img/industrial/piezas-industriales/002.jpg",
+					"img/industrial/piezas-industriales/003.jpg",
+					"img/industrial/piezas-industriales/004.png",
+					"img/industrial/piezas-industriales/005.png"
 				)
-			)
+			),
+		"i04" => array(
+				"info" => "Tanques PRFV",
+				"data" => "img/industrial/tanques/thumb.jpg",
+				"informacion" => "Tanques multiproposito",
+				"descrip" => "Revestimientos antiácidos, celdas electrolíticas, reactores, tanques, laboratorios de cultivo, tanques de tratamiento de agua. Celdas de concreto polimérico. Recuperación de Cobre, zonc, plomo, cromo. Garantia, calidad, durabilidad, ingeniería.",
+				"fotosExtras" => array(
+					"img/industrial/tanques/001.jpg",
+					"img/industrial/tanques/002.jpg",
+					"img/industrial/tanques/003.jpg",
+					"img/industrial/tanques/004.jpg",
+					"img/industrial/tanques/005.jpg",
+					"img/industrial/tanques/006.jpg",
+					"img/industrial/tanques/007.jpg",
+					"img/industrial/tanques/008.jpg",
+					"img/industrial/tanques/009.jpg",
+					"img/industrial/tanques/010.jpg",
+					"img/industrial/tanques/011.jpg",
+					"img/industrial/tanques/012.jpg",
+					"img/industrial/tanques/013.jpg"
+				)
+			),
 	);
 ?>
